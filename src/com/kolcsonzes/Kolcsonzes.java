6f8b879f42c024c75945230f34bd161d4e6ec40a
pulekifeter <104682834+pulekifeter@users.noreply.github.com>
2022-05-17 15:21:31 +0200

    konyv kolcsonzes gui
    gui

---
+++ src/com/kolcsonzes/Kolcsonzes.java
@@ -0,0 +1,14 @@
+package com.kolcsonzes;
+
+import javax.swing.*;
+
+public class Kolcsonzes {
+    private JPanel panel1;
+    private JTable table1;
+    private JButton button1;
+    private JButton button2;
+    private JTable table2;
+    private JTable table3;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+}
