843bd392264c078a6f767a285bfbfb754e6d7059
pulekifeter <104682834+pulekifeter@users.noreply.github.com>
2022-05-17 13:33:16 +0200

    Leltar modositas
    Leltari konyv keresese gui

---
+++ src/com/leltar/LeltarKereses.java
@@ -0,0 +1,11 @@
+package com.leltar;
+
+import javax.swing.*;
+
+public class LeltarKereses {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton button1;
+    private JComboBox comboBox1;
+    private JTable table1;
+}
