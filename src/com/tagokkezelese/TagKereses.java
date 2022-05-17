48b09700d8a1e5eb6bca43bbee477a8a968e7edd
pulekifeter <104682834+pulekifeter@users.noreply.github.com>
2022-05-17 13:18:22 +0200

    Tagok keresese
    Tagok keresese gui

---
+++ src/com/tagokkezelese/TagKereses.java
@@ -0,0 +1,10 @@
+package com.tagokkezelese;
+
+import javax.swing.*;
+
+public class TagKereses {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton button1;
+    private JTable table1;
+}
