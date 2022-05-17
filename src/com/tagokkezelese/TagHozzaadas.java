040db3a3b734a81efc7622f649d2d32b1cff9ad6
pulekifeter <104682834+pulekifeter@users.noreply.github.com>
2022-05-17 13:16:20 +0200

    Tagok kezelese
    Tag hozzaadasa gui

---
+++ src/com/tagokkezelese/TagHozzaadas.java
@@ -0,0 +1,13 @@
+package com.tagokkezelese;
+
+import javax.swing.*;
+
+public class TagHozzaadas {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton felvételButton;
+    private JButton mégsemButton;
+}
