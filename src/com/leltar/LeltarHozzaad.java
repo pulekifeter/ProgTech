e876bc0bd8ff146aaae75a37bf8112dfdb262c41
pulekifeter <104682834+pulekifeter@users.noreply.github.com>
2022-05-17 13:25:56 +0200

    leltar elemek
    Konyv leltarhoz adasa gui

---
+++ src/com/leltar/LeltarHozzaad.java
@@ -0,0 +1,10 @@
+package com.leltar;
+
+import javax.swing.*;
+
+public class LeltarHozzaad {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTable table1;
+    private JButton hozzáadásButton;
+}
