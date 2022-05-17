1f50c3bb6348d4b16326ff4bcbe02d24dec4b634
pulekifeter <104682834+pulekifeter@users.noreply.github.com>
2022-05-17 10:54:36 +0200

    Konyvallomany folytatas
    Konyv modositasa gui

---
+++ src/com/konyvallomany/KonyvModositas.java
@@ -0,0 +1,10 @@
+package com.konyvallomany;
+
+import javax.swing.*;
+
+public class KonyvModositas {
+    private JPanel panel1;
+    private JTable table1;
+    private JButton szerkesztésButton;
+    private JButton mégsemButton;
+}
