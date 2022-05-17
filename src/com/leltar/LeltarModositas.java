a228c9257768887460fb8b2fd76169c2c9e77b0a
pulekifeter <104682834+pulekifeter@users.noreply.github.com>
2022-05-17 15:12:11 +0200

    Leltar GUI folytatas
    Leltar modositasa gui

---
+++ src/com/leltar/LeltarModositas.java
@@ -0,0 +1,10 @@
+package com.leltar;
+
+import javax.swing.*;
+
+public class LeltarModositas {
+    private JPanel panel1;
+    private JTable table1;
+    private JButton módosításButton;
+    private JButton mégsemButton;
+}
