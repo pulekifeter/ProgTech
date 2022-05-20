a4f63242dbc3b2aeaabfb2a420cd72954942900c
pulekifeter <104682834+pulekifeter@users.noreply.github.com>
2022-05-20 10:15:00 +0200

    Update FoOldal.java

--- src/com/fooldal/FoOldal.java
+++ src/com/fooldal/FoOldal.java
@@ -13,11 +13,11 @@ import com.loginpage.LoginPage;
 import com.tagokkezelese.TagHozzaadas;
 import com.tagokkezelese.TagKereses;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import javax.swing.*;
 
 public class FoOldal {
     public JMenuBar createMenuBar(){
